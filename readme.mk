1704b8a00e36a6c1adfdbd614b46f19cb6b48b4d
Rado Kirov <radokirov@google.com>
2015-03-05 22:05:51 -0800

    clean-up readme.

--- readme.mk
+++ readme.mk
@@ -1,35 +1,26 @@
-# Getting started
-1) run `./build.sh`
-2) serve
+# Components
+- App
+- SearchResult
+- eitherpanel
+- thumbs
 
-# components
-app
- - search
- - videos ([Video])
-   - video (Video)
-	   - player
-		 - eitherpanel (left - right)
-	   - thumbs up/down
+# Sharp Edges
+The application does not fully work out-of-the-box without the following patches.
 
-[x] add events
-[x] sync with npm
-[x] test with es5 compiled output
-[x] use form API
-[x] make transclusion work with emulated shadow dom (will solve video problem)
-[x] get bootstrap glyphs to show (or other thumbs)
-[x] insert google-youtube player into component
-[x] bind to video properties
-[x] switch back to emulated mode
-[ ] replace as much as possible cdn with localhost
-
-# patches
+## need to be patched
 - zone.js
-	- https://github.com/angular/zone.js/issues/52 (patch locally in node_modules/zone.js)
-- google-youtube element
+	- https://github.com/angular/zone.js/issues/52 (need to patch locally in node_modules/zone.js)
+- angular 2
+	- https://github.com/angular/angular/issues/776 (need to patch locally)
+
+## patches already in the repo
+- google-youtube web component
 	- support property changes (add bug here)
 	- incorrectly determines player support (add bug here)
-	- patched locally
+	- patched locally and checked in bower_components
 - youtube api
-	- uses a custom promise lib that zone.js doesn't support (patch in patch_youtube_client.js)
-- angular 2
-	- https://github.com/angular/angular/issues/776
+	- uses a custom promise lib that zone.js doesn't support (patched in patch_youtube_client.js)
+
+## google youtube api key
+- Will be revoked soon after application is public.
+	You will need to provide your own key if you want to run the app.
